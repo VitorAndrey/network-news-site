d9a13afa101a0eabe3dc932e6dcb68414d0fdbd3
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-09-17 17:13:27 -0300

    adicionei o componente de dialogo

--- app/galery/components/responsive-gallery.tsx
+++ app/galery/components/responsive-gallery.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { useRef, useState } from "react";
+
 import Image from "next/image";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+
+// import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 import { Asset } from "../page";
 
@@ -10,6 +14,17 @@ type ResponsiveGalleryProps = {
 };
 
 export function ResponsiveGallery({ assets }: ResponsiveGalleryProps) {
+  const [data, setData] = useState({ asset: { type: "", url: "" }, i: 0 });
+  const openDialog = useRef<HTMLButtonElement | null>(null);
+
+  function handleViewAsset(item: Asset, i: number) {
+    setData({ asset: { type: item.type, url: item.asset }, i });
+
+    if (openDialog.current) {
+      openDialog.current.click();
+    }
+  }
+
   return (
     <section className="p-8">
       <ResponsiveMasonry
@@ -27,15 +42,18 @@ export function ResponsiveGallery({ assets }: ResponsiveGalleryProps) {
                     height={200}
                     style={{ width: "100%", display: "block" }}
                     alt="Gallery Image"
-                    className="rounded-lg"
+                    className="rounded-lg cursor-pointer"
+                    onClick={() => handleViewAsset(item, i)}
                   />
                 )}
                 {item.type === "video" && (
                   <video
+                    className="rounded-lg cursor-pointer"
                     loop
                     autoPlay
                     muted
                     style={{ width: "100%", display: "block" }}
+                    onClick={() => handleViewAsset(item, i)}
                   >
                     <source src={item.asset} type="video/mp4" />
                   </video>
@@ -47,6 +65,25 @@ export function ResponsiveGallery({ assets }: ResponsiveGalleryProps) {
           )}
         </Masonry>
       </ResponsiveMasonry>
+
+      {/* <Dialog>
+        <DialogTrigger ref={openDialog} />
+        <DialogContent>
+          {data.asset.type === "image" && (
+            <Image
+              src={data.asset.url}
+              width={300}
+              height={200}
+              alt="Gallery Image"
+            />
+          )}
+          {data.asset.type === "video" && (
+            <video className="rounded-lg cursor-pointer" controls>
+              <source src={data.asset.url} type="video/mp4" />
+            </video>
+          )}
+        </DialogContent>
+      </Dialog> */}
     </section>
   );
 }
